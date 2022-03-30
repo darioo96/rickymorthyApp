2417fc02f61efac6b421ba7946941779e1bb7e80
Dario Ortega <dariolean10@gmail.com>
2022-03-30 15:11:07 -0300

    footer links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,14 @@ function Footer() {
   return (
     <footer>
         <div className="contactos">
-            <h3>Darío Ortega</h3>
+            <p>Darío Ortega</p>
             <div className="links">
-                <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/1384/1384055.png" alt="" /></a>
-                <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/1384/1384062.png" alt="" /></a> 
+                <a href="https://walink.co/0916d3">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1384/1384055.png" alt="" />
+                </a>
+                <a href="https://www.linkedin.com/in/darioortega96/">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1384/1384062.png" alt="" />
+                </a> 
             </div>
            
         </div>
